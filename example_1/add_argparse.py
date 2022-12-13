ab712224cc47938d41d14fdc6025b4f0aeac120f
Nelson Cárdenas Bolaño <nelsoncardenas28@gmail.com>
2022-12-13 11:33:40 -0500

    chore(example_1):  create add scripts

---
+++ example_1/add_argparse.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def add_two_numbers(number1: int, number2: int):
+    """Adds two numbers together and prints the result."""
+    print(number1 + number2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number1", type=int)
+    parser.add_argument("number2", type=int)
+    args = parser.parse_args()
+
+    add_two_numbers(args.number1, args.number2)
